579500aad1d1b983f9e0872f12701bd7fe4cd395
David Woods <woodsd@protrans.com>
2023-03-21 18:11:46 -0600

    cleanup

--- EdiParser.Tests.Functional/Edi204MotorCarrierLoadTenderTests.cs
+++ EdiParser.Tests.Functional/Edi204MotorCarrierLoadTenderTests.cs
@@ -1,6 +1,7 @@
 ﻿using EdiParser.x12;
 using EdiParser.x12.DomainModels;
 using EdiParser.x12.DomainModels._204;
+using EdiParser.x12.Mapping;
 using EdiParser.x12.Models;
 using Xunit.Abstractions;
 
@@ -14,6 +15,126 @@ public class Edi204MotorCarrierLoadTenderTests
     {
         _output = output;
         //Logging.LoggerFactory = new XUnitLoggingFactory(output);
+    }
+
+    [Theory]
+    [ClassData(typeof(Edi204TestDataGenerator))]
+    public void LoadFiles(string fileName, Edi204_MotorCarrierLoadTender expected)
+    {
+        var data = File.ReadAllText(fileName);
+        var document = x12Document.Parse(data);
+
+        var edi204 = new Edi204_MotorCarrierLoadTender();
+        edi204.LoadFrom(document.Sections[0]);
+
+        Assert.Equivalent(expected.Entities[0], edi204.Entities[0], true);
+        Assert.Equivalent(expected.Entities[1], edi204.Entities[1], true);
+
+        Assert.Equivalent(expected.Stops[0].Details, edi204.Stops[0].Details, true);
+        Assert.Equivalent(expected.Stops[0], edi204.Stops[0], true);
+        Assert.Equivalent(expected.Stops[1], edi204.Stops[1], true);
+
+        Assert.Equivalent(expected, edi204, true);
+    }
+
+
+    [Theory]
+    [InlineData(@"G:\EdiSamples\LandstarAgent\204\OUT", '*', '~')]
+    [InlineData(@"G:\EdiSamples\PureTransportation\204\OUT\2023\02", '*', '~')]
+    [InlineData(@"G:\EdiSamples\R2Logistics\204\OUT\2023\02", '*', '~')]
+    [InlineData(@"G:\EdiSamples\Marelli\204\IN\01", '*', '~')]
+    [InlineData(@"G:\EdiSamples\Penske\204\IN\02\WPIB", '^', '~')]
+    [InlineData(@"G:\EdiSamples\KLGExpress\204\OUT\2023\02", '*', '~')]
+    [InlineData(@"G:\EdiSamples\FreightSOL\204\OUT\2023\02", '*', '~')]
+    [InlineData(@"G:\EdiSamples\CardinalLogisticsManagement\204\OUT\2023\02", '*', '~')]
+    [InlineData(@"G:\EdiSamples\CHRobinson\204\OUT", '*', '~')]
+    [InlineData(@"G:\EdiSamples\Transplace\204\IN\02", '*', '~')]
+    [InlineData(@"G:\EdiSamples\knight\204\OUT\2023\02", '*', '~')]
+    [InlineData(@"G:\EdiSamples\Penske\204\IN\02\ZF", '*', '\n')]
+    [InlineData(@"G:\EdiSamples\CHRobinson\204\IN", '*', '\n')]
+    public void ToDocument(string path, char seperator, char lineSeperator)
+    {
+        foreach (var file in Directory.GetFiles(path))
+        {
+            var expectedLines = new string[] { };
+            var actualLines = new string[] { };
+
+            try
+            {
+                var data = File.ReadAllText(file);
+
+
+                // var data = @"ISA*01*0000000000*01*0000000000*ZZ*ABCDEFGHIJKLMNO*ZZ*123456789012345*101127*1719*U*00400*000003438*0*P*>~
+                //              GS*SM*4405197800*999999999*20111219*1747*2100*X*004010~
+                //              ST*204*0001~
+                //              B2**XXXX**9999955559**PP~
+                //              B2A*04~
+                //              L11*NONPRIMARY*OK~
+                //              NTE**FROZEN GOODS SET TO -10d F~
+                //              N1*PF*XYZ CORP*9*9995555500000~
+                //              N3*31875 SOLON RD~
+                //              N4*SOLON*OH*44139~
+                //                 N7**NONE*********FF****5300~
+                //              S5*1*CL*27800*L*2444*CA*1016*E~
+                //              L11*9999001947*DO~
+                //              L11*9999670098*CR~
+                //              L11*9999001866*DO~
+                //              L11*9999669887*CR~".Replace("\t", "").Replace(" ", "");
+
+                var document = x12Document.Parse(data);
+
+                var documentSections = new List<Section>();
+                foreach (var section in document.Sections)
+                {
+                    var edi204 = new Edi204_MotorCarrierLoadTender();
+                    edi204.LoadFrom(section);
+                    documentSections.Add(edi204.ToDocumentSection(section.TransactionSetControlNumber));
+                }
+
+
+                var newDocument = new x12Document();
+                newDocument.IsaInterchangeControlHeader = document.IsaInterchangeControlHeader;
+                newDocument.GsHeader = document.GsHeader;
+                foreach (var documentSection in documentSections) newDocument.Sections.Add(documentSection);
+
+
+                var outputOptions = new MapOptions();
+                outputOptions.Separator = seperator.ToString();
+                outputOptions.LineEnding = lineSeperator.ToString();
+
+                var actual = newDocument.ToString(outputOptions);
+                //actual = actual.Replace("~", "~\r\n"); //pretty formatting
+
+                expectedLines = data.Replace("\r\n", "\n").Split(lineSeperator);
+                actualLines = actual.Split(lineSeperator);
+
+
+                for (var i = 0; i < expectedLines.Length; i++)
+                {
+                    if (expectedLines[i].StartsWith("SE")) //everyone implements the counts differently
+                        continue;
+                    //remove all whitespace for the comparison as some of the test EDI files have trailing whitespace which the library would not replicate
+                    Assert.Equal(expectedLines[i].Trim().Replace(" ", ""), actualLines[i].Trim().Replace(" ", "")); //<object> shows more output if this is being truncated
+                }
+            }
+            catch (Exception)
+            {
+                _output.WriteLine(file);
+                for (var i = 0; i < Math.Min(expectedLines.Length, actualLines.Length); i++)
+                {
+                    if (i == 0) continue;
+                    if (expectedLines[i].Trim().Replace(" ", "") != actualLines[i].Trim().Replace(" ", ""))
+                    {
+                        _output.WriteLine($"[{(i + 1).ToString().PadLeft(2, '0')}] {expectedLines[i].Trim().PadRight(50)} {actualLines[i].Trim()}");
+                        break; //exit when the line does not match (but print the one that did not match
+                    }
+
+                    _output.WriteLine($"[{(i + 1).ToString().PadLeft(2, '0')}] {expectedLines[i].Trim().PadRight(50)} {actualLines[i].Trim()}");
+                }
+
+                throw;
+            }
+        }
     }
 
     [Fact]
@@ -45,8 +166,8 @@ public class Edi204MotorCarrierLoadTenderTests
         var expected = new Section();
         expected.Segments.Add(new B2_BeginningSegmentForShipmentInformationTransaction { StandardCarrierAlphaCode = sourceModel.ShipmentInformation.StandardCarrierAlphaCode, ShipmentIdentificationNumber = sourceModel.ShipmentInformation.ShipmentIdentificationNumber, ShipmentMethodOfPaymentCode = sourceModel.ShipmentInformation.ShipmentMethodOfPaymentCode });
         expected.Segments.Add(new B2A_SetPurpose { TransactionSetPurposeCode = sourceModel.Purpose });
-        expected.Segments.Add(new L11_BusinessInstructionsAndReferenceNumber { ReferenceIdentification = sourceModel.ReferenceNumbers[0].ReferenceIdentification, ReferenceIdentificationQualifier = sourceModel.ReferenceNumbers[0].ReferenceIdentificationQualifier });
-        expected.Segments.Add(new L11_BusinessInstructionsAndReferenceNumber { ReferenceIdentification = sourceModel.ReferenceNumbers[1].ReferenceIdentification, ReferenceIdentificationQualifier = sourceModel.ReferenceNumbers[1].ReferenceIdentificationQualifier });
+        expected.Segments.Add(new L11_BusinessInstructionsAndReferenceNumber { ReferenceIdentification = sourceModel.ReferenceNumbers[0].Description, ReferenceIdentificationQualifier = sourceModel.ReferenceNumbers[0].ReferenceIdentificationQualifier });
+        expected.Segments.Add(new L11_BusinessInstructionsAndReferenceNumber { ReferenceIdentification = sourceModel.ReferenceNumbers[1].Description, ReferenceIdentificationQualifier = sourceModel.ReferenceNumbers[1].ReferenceIdentificationQualifier });
         expected.Segments.Add(new NTE_Note { Description = sourceModel.Notes[0].Description, NoteReferenceCode = sourceModel.Notes[0].NoteReferenceCode });
         expected.Segments.Add(new NTE_Note { Description = sourceModel.Notes[1].Description, NoteReferenceCode = sourceModel.Notes[1].NoteReferenceCode });
         expected.Segments.Add(new N1_PartyIdentification
---
+++ EdiParser.Tests/x12/Beta/GroupLang.cs
@@ -0,0 +1,45 @@
+﻿using EdiParser.x12;
+using EdiParser.x12.Models.Internals;
+
+namespace EdiParser.Tests.x12.Beta;
+
+public class GroupLang
+{
+    public static GroupingRule[] ToGroups(string pattern)
+    {
+        var groups = new List<GroupingRule>();
+
+        var rootIndentation = 0; //store the base elements indentation. Everything that is +2 over the indent it a new group
+        foreach (var line in pattern.Split(Environment.NewLine))
+        {
+            if (line.Trim() == "+++")
+                continue;
+            
+            if (rootIndentation == 0) //first line we encounter
+                rootIndentation = line.IndexOf("+");
+
+            if (line.Substring(rootIndentation, 1) == "+")
+            {
+
+            }
+
+
+            var data = line.Trim().Substring(1); //strip off the leading character
+            var name = data.Substring(0, data.IndexOf("(")-1).Trim();
+            var parts = data.Substring(data.IndexOf("(")+1).Trim();
+            parts = parts.Substring(0, parts.Length - 1); //strip off closing )
+
+            var allowedTypeString = parts.Split(",").Select(x=>x.Trim());
+            var allowedTypes = new List<Type>();
+            foreach (var allowedType in allowedTypeString)
+            {
+                allowedTypes.Add(EdiSectionParserFactory.GetSegmentFor(allowedType));
+            }
+            var rule = new GroupingRule(name, allowedTypes.ToArray());
+            
+            groups.Add(rule);
+        }
+
+        return groups.ToArray();
+    }
+}
