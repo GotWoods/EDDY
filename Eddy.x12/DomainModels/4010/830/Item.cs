1ba0dd96815018131ace9e448b0f46b21fe9f496
David Woods <woodsd@protrans.com>
2023-04-11 16:57:14 -0600

    bit more work on 830

---
+++ Eddy.x12/DomainModels/4010/830/Item.cs
@@ -0,0 +1,12 @@
+﻿using Eddy.Core.Attributes;
+using Eddy.x12.Models;
+
+namespace Eddy.x12.DomainModels._4010._830;
+
+public class Item
+{
+    [SectionPosition(1)] public LIN_ItemIdentification ItemIdentification { get; set; }
+    //[SectionPosition(2)] public UIT
+    [SectionPosition(3)] DTM_DateTimeReference dateTimeReferences { get; set; }
+    //[SectionPosition(1)] PO3
+}
---
+++ Eddy.x12/Models/PDH_TODO.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Eddy.x12.Models
+{
+    //TODO: this class is not auto generating
+    public class PDH_TODO
+    {
+    }
+}
