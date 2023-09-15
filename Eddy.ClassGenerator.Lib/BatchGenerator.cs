8332376615c42c4796f50d39632dc293e677c4ff
Dave Woods <none@none.com>
2023-09-15 14:59:06 -0600

    FOB-G08

--- Eddy.ClassGenerator.Lib/BatchGenerator.cs
+++ Eddy.ClassGenerator.Lib/BatchGenerator.cs
@@ -114,7 +114,7 @@ public class BatchGenerator
 
         var codeBasePath = projectBasePath + @"Eddy.x12\Models";
         var testBasePath = projectBasePath + @"Eddy.Tests.x12\Models";
-        var ignored = new List<string>() {"ADJ", "B10", "CD", "CTP", "D1", "CUR", "F04", "F07"}; //ones that are not generating very well
+        var ignored = new List<string>() {"ADJ", "B10", "CD", "CTP", "D1", "CUR", "F04", "F07", "FK"}; //ones that are not generating very well
         //CTP/F04 is causing an Object Reference exception
 
         //make sure directories exist for all versions
