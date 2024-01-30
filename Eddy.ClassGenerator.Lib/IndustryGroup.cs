46c65078805ab83eef0fb1d09a75b9b3b92f1c15
Dave Woods <none@none.com>
2024-01-30 07:12:10 -0700

    remove class gen

--- Eddy.ClassGenerator.Lib/IndustryGroup.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections.Generic;
-
-namespace Eddy.ClassGenerator.Lib;
-
-public class IndustryGroup
-{
-    public string Name { get; set; }
-    public List<SegmentData> TransactionSets { get; set; } = new();
-}
--- Eddy.ClassGenerator.Lib/ParseType.cs
+++
@@ -1,9 +0,0 @@
-namespace Eddy.ClassGenerator.Lib;
-
-public enum ParseType
-{
-    x12Segment,
-    x12Element,
-    ediFactElement,
-    ediFactSegment
-}
