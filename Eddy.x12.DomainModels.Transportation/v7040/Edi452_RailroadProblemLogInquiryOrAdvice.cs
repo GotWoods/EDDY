6f75a77fa5804199c01574e5035dfa4a706ed11a
Dave Woods <none@none.com>
2024-02-04 11:21:18 -0700

    more namespaces

---
+++ Eddy.x12.DomainModels.Transportation/v7040/Edi452_RailroadProblemLogInquiryOrAdvice.cs
@@ -0,0 +1,33 @@
+using System.Collections.Generic;
+using Eddy.Core.Attributes;
+using Eddy.Core.Validation;
+using Eddy.x12.Models.v7040;
+
+namespace Eddy.x12.DomainModels.Transportation.v7040;
+
+public class Edi452_RailroadProblemLogInquiryOrAdvice {
+	[SectionPosition(1)] public ST_TransactionSetHeader TransactionSetHeader { get; set; } = new();
+	[SectionPosition(2)] public BGP_BeginningSegmentForProblemLogInquiryOrAdvice BeginningSegmentForProblemLogInquiryOrAdvice { get; set; } = new();
+	[SectionPosition(3)] public N1_PartyIdentification? PartyIdentification { get; set; }
+	[SectionPosition(4)] public List<N9_ExtendedReferenceInformation> ExtendedReferenceInformation { get; set; } = new();
+	[SectionPosition(5)] public List<DTP_DateOrTimeOrPeriod> DateOrTimeOrPeriod { get; set; } = new();
+	[SectionPosition(6)] public List<NTE_NoteSpecialInstruction> NoteSpecialInstruction { get; set; } = new();
+	[SectionPosition(7)] public SE_TransactionSetTrailer TransactionSetTrailer { get; set; } = new();
+
+	//Details
+
+	//Summary
+
+
+	public ValidationResult Validate()
+	{
+		var validator = new TransactionValidator<Edi452_RailroadProblemLogInquiryOrAdvice>(this);
+		validator.Required(x => x.TransactionSetHeader);
+		validator.Required(x => x.BeginningSegmentForProblemLogInquiryOrAdvice);
+		validator.CollectionSize(x => x.ExtendedReferenceInformation, 0, 5);
+		validator.CollectionSize(x => x.DateOrTimeOrPeriod, 0, 2);
+		validator.CollectionSize(x => x.NoteSpecialInstruction, 0, 999999);
+		validator.Required(x => x.TransactionSetTrailer);
+		return validator.Results;
+	}
+}
---
+++ Eddy.x12.DomainModels.Transportation/v8030/Edi998_SetCancellation.cs
@@ -0,0 +1,26 @@
+using System.Collections.Generic;
+using Eddy.Core.Attributes;
+using Eddy.Core.Validation;
+using Eddy.x12.Models.v8030;
+
+namespace Eddy.x12.DomainModels.Transportation.v8030;
+
+public class Edi998_SetCancellation {
+	[SectionPosition(1)] public ST_TransactionSetHeader TransactionSetHeader { get; set; } = new();
+	[SectionPosition(2)] public ZD_TransactionSetDeletionIDReasonAndSource TransactionSetDeletionIDReasonAndSource { get; set; } = new();
+	[SectionPosition(3)] public SE_TransactionSetTrailer TransactionSetTrailer { get; set; } = new();
+
+	//Details
+
+	//Summary
+
+
+	public ValidationResult Validate()
+	{
+		var validator = new TransactionValidator<Edi998_SetCancellation>(this);
+		validator.Required(x => x.TransactionSetHeader);
+		validator.Required(x => x.TransactionSetDeletionIDReasonAndSource);
+		validator.Required(x => x.TransactionSetTrailer);
+		return validator.Results;
+	}
+}
