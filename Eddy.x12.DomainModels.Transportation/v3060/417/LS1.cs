13745acb05a7b2f3346e96d0d7ac51c95a3e605c
Dave Woods <none@none.com>
2024-02-04 10:41:22 -0700

    removing failed validations

--- Eddy.x12.DomainModels.Transportation/v3060/417/LS1.cs
+++ Eddy.x12.DomainModels.Transportation/v3060/417/LS1.cs
@@ -15,15 +15,15 @@ public class LS1 {
 	[SectionPosition(7)] public N4_GeographicLocation? GeographicLocation { get; set; }
 	[SectionPosition(8)] public PER_AdministrativeCommunicationsContact? AdministrativeCommunicationsContact { get; set; }
 	[SectionPosition(9)] public LS_LoopHeader? LoopHeader { get; set; }
-	[SectionPosition(10)] public List<LS1_LS1> LS1 {get;set;} = new();
+	[SectionPosition(10)] public List<LS1_LS1> LS1_LS1 { get;set;} = new();
 	[SectionPosition(11)] public LE_LoopTrailer? LoopTrailer { get; set; }
 	public ValidationResult Validate()
 	{
 		var validator = new TransactionValidator<LS1>(this);
 		validator.Required(x => x.StopOffName);
 		validator.CollectionSize(x => x.StopOffAddress, 0, 2);
-		validator.CollectionSize(x => x.LS1, 0, 12);
-		foreach (var item in LS1) validator.Results.AddRange(item.Validate().Errors);
+		validator.CollectionSize(x => x.LS1_LS1, 0, 12);
+		foreach (var item in LS1_LS1) validator.Results.AddRange(item.Validate().Errors);
 		return validator.Results;
 	}
 }
