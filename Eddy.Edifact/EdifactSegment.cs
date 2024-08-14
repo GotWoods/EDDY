26c8049e0e708892bbd9eb048e0624c0eceb0cf7
Dave Woods <none@none.com>
2024-08-14 12:43:21 -0600

    Adding new EdifactSegment

---
+++ Eddy.Edifact/EdifactSegment.cs
@@ -0,0 +1,12 @@
+﻿using Eddy.Core.Validation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Eddy.Edifact
+{
+    public abstract class EdifactSegment
+    {
+        public abstract ValidationResult Validate();
+    }
+}
