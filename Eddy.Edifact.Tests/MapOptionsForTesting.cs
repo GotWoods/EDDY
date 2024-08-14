3338cb34aeb34db9ba3de63e36cabad659b234a9
Dave Woods <none@none.com>
2024-08-14 14:04:59 -0600

    Adding options for EdiFact

---
+++ Eddy.Edifact.Tests/MapOptionsForTesting.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Eddy.Edifact.Mapping;
+
+namespace Eddy.Edifact.Tests
+{
+    public class MapOptionsForTesting
+    {
+        public static MapOptions EdifactDefaultEndsWithSingleQuote
+        {
+            get
+            {
+                return new MapOptions() { LineEnding = "'", Separator = "+", StandardsVersion = "D21A", ComponentElementSeparator = ":" };
+            }
+        }
+    }
+}
