35e1b2ebc55bae51ea5a201844245430c44bfa66
Dave Woods <dave@solidhouse.com>
2024-08-30 23:19:12 -0600

    batch of EdiFact.Transport

---
+++ Eddy.Edifact.DomainModels.Transport/D00A/COPRAR/SegmentGroup6_SegmentGroup9.cs
@@ -0,0 +1,23 @@
+using System.Collections.Generic;
+using Eddy.Core.Attributes;
+using Eddy.Core.Validation;
+using Eddy.Edifact.Models.D00A;
+
+namespace Eddy.Edifact.DomainModels.Transport.D00A.COPRAR;
+
+public class SegmentGroup6_SegmentGroup9 {
+	[SectionPosition(1)] public TDT_DetailsOfTransport DetailsOfTransport { get; set; } = new();
+	[SectionPosition(2)] public List<DTM_DateTimePeriod> DateTimePeriod { get; set; } = new();
+	[SectionPosition(3)] public List<RFF_Reference> Reference { get; set; } = new();
+	[SectionPosition(4)] public List<SegmentGroup6__SegmentGroup9_SegmentGroup10> SegmentGroup10 {get;set;} = new();
+	public ValidationResult Validate()
+	{
+		var validator = new TransactionValidator<SegmentGroup6_SegmentGroup9>(this);
+		validator.Required(x => x.DetailsOfTransport);
+		validator.CollectionSize(x => x.DateTimePeriod, 1, 9);
+		validator.CollectionSize(x => x.Reference, 1, 9);
+		validator.CollectionSize(x => x.SegmentGroup10, 0, 9);
+		foreach (var item in SegmentGroup10) validator.Results.AddRange(item.Validate().Errors);
+		return validator.Results;
+	}
+}
