1465a3b47027552e21ee74f85ee2737e3e2f4139
Dave Woods <dave@solidhouse.com>
2024-08-30 23:20:23 -0600

    batch of EdiFact.Transport

---
+++ Eddy.Edifact.DomainModels.Transport/D00A/COSTCO/SegmentGroup7__SegmentGroup8_SegmentGroup10.cs
@@ -0,0 +1,26 @@
+using System.Collections.Generic;
+using Eddy.Core.Attributes;
+using Eddy.Core.Validation;
+using Eddy.Edifact.Models.D00A;
+
+namespace Eddy.Edifact.DomainModels.Transport.D00A.COSTCO;
+
+public class SegmentGroup7__SegmentGroup8_SegmentGroup10 {
+	[SectionPosition(1)] public DGS_DangerousGoods DangerousGoods { get; set; } = new();
+	[SectionPosition(2)] public List<FTX_FreeText> FreeText { get; set; } = new();
+	[SectionPosition(3)] public List<MEA_Measurements> Measurements { get; set; } = new();
+	[SectionPosition(4)] public List<SegmentGroup7__SegmentGroup8__SegmentGroup10_SegmentGroup11> SegmentGroup11 {get;set;} = new();
+	[SectionPosition(5)] public List<SegmentGroup7__SegmentGroup8__SegmentGroup10_SegmentGroup12> SegmentGroup12 {get;set;} = new();
+	public ValidationResult Validate()
+	{
+		var validator = new TransactionValidator<SegmentGroup7__SegmentGroup8_SegmentGroup10>(this);
+		validator.Required(x => x.DangerousGoods);
+		validator.CollectionSize(x => x.FreeText, 1, 9);
+		validator.CollectionSize(x => x.Measurements, 1, 9);
+		validator.CollectionSize(x => x.SegmentGroup11, 0, 9);
+		validator.CollectionSize(x => x.SegmentGroup12, 0, 999);
+		foreach (var item in SegmentGroup11) validator.Results.AddRange(item.Validate().Errors);
+		foreach (var item in SegmentGroup12) validator.Results.AddRange(item.Validate().Errors);
+		return validator.Results;
+	}
+}
---
+++ Eddy.Edifact.DomainModels.Transport/D00A/COSTOR/SegmentGroup8.cs
@@ -0,0 +1,23 @@
+using System.Collections.Generic;
+using Eddy.Core.Attributes;
+using Eddy.Core.Validation;
+using Eddy.Edifact.Models.D00A;
+
+namespace Eddy.Edifact.DomainModels.Transport.D00A.COSTOR;
+
+public class SegmentGroup8 {
+	[SectionPosition(1)] public CNI_ConsignmentInformation ConsignmentInformation { get; set; } = new();
+	[SectionPosition(2)] public List<RFF_Reference> Reference { get; set; } = new();
+	[SectionPosition(3)] public List<NAD_NameAndAddress> NameAndAddress { get; set; } = new();
+	[SectionPosition(4)] public List<SegmentGroup8_SegmentGroup9> SegmentGroup9 {get;set;} = new();
+	public ValidationResult Validate()
+	{
+		var validator = new TransactionValidator<SegmentGroup8>(this);
+		validator.Required(x => x.ConsignmentInformation);
+		validator.CollectionSize(x => x.Reference, 1, 9);
+		validator.CollectionSize(x => x.NameAndAddress, 1, 9);
+		validator.CollectionSize(x => x.SegmentGroup9, 0, 999);
+		foreach (var item in SegmentGroup9) validator.Results.AddRange(item.Validate().Errors);
+		return validator.Results;
+	}
+}
