ee147e263b0c34d9caca8b6a52554cd8cdb3c45e
Dave Woods <dave@solidhouse.com>
2024-08-30 23:26:20 -0600

    batch of EdiFact.Transport

---
+++ Eddy.Edifact.DomainModels.Transport/D00A/IFTMBP/SegmentGroup14_SegmentGroup16.cs
@@ -0,0 +1,18 @@
+using System.Collections.Generic;
+using Eddy.Core.Attributes;
+using Eddy.Core.Validation;
+using Eddy.Edifact.Models.D00A;
+
+namespace Eddy.Edifact.DomainModels.Transport.D00A.IFTMBP;
+
+public class SegmentGroup14_SegmentGroup16 {
+	[SectionPosition(1)] public MEA_Measurements Measurements { get; set; } = new();
+	[SectionPosition(2)] public EQN_NumberOfUnits NumberOfUnits { get; set; } = new();
+	public ValidationResult Validate()
+	{
+		var validator = new TransactionValidator<SegmentGroup14_SegmentGroup16>(this);
+		validator.Required(x => x.Measurements);
+		validator.Required(x => x.NumberOfUnits);
+		return validator.Results;
+	}
+}
---
+++ Eddy.Edifact.DomainModels.Transport/D00A/IFTMBP/SegmentGroup8_SegmentGroup10.cs
@@ -0,0 +1,18 @@
+using System.Collections.Generic;
+using Eddy.Core.Attributes;
+using Eddy.Core.Validation;
+using Eddy.Edifact.Models.D00A;
+
+namespace Eddy.Edifact.DomainModels.Transport.D00A.IFTMBP;
+
+public class SegmentGroup8_SegmentGroup10 {
+	[SectionPosition(1)] public DOC_DocumentMessageDetails DocumentMessageDetails { get; set; } = new();
+	[SectionPosition(2)] public DTM_DateTimePeriod DateTimePeriod { get; set; } = new();
+	public ValidationResult Validate()
+	{
+		var validator = new TransactionValidator<SegmentGroup8_SegmentGroup10>(this);
+		validator.Required(x => x.DocumentMessageDetails);
+		validator.Required(x => x.DateTimePeriod);
+		return validator.Results;
+	}
+}
