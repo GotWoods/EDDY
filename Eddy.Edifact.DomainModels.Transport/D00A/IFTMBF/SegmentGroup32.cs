1166313fd6aae37c8ad994aa977c36462c8450e2
Dave Woods <dave@solidhouse.com>
2024-08-30 23:24:19 -0600

    batch of EdiFact.Transport

---
+++ Eddy.Edifact.DomainModels.Transport/D00A/IFTMBF/SegmentGroup32.cs
@@ -0,0 +1,40 @@
+using System.Collections.Generic;
+using Eddy.Core.Attributes;
+using Eddy.Core.Validation;
+using Eddy.Edifact.Models.D00A;
+
+namespace Eddy.Edifact.DomainModels.Transport.D00A.IFTMBF;
+
+public class SegmentGroup32 {
+	[SectionPosition(1)] public EQD_EquipmentDetails EquipmentDetails { get; set; } = new();
+	[SectionPosition(2)] public EQN_NumberOfUnits NumberOfUnits { get; set; } = new();
+	[SectionPosition(3)] public TMD_TransportMovementDetails TransportMovementDetails { get; set; } = new();
+	[SectionPosition(4)] public List<MEA_Measurements> Measurements { get; set; } = new();
+	[SectionPosition(5)] public List<DIM_Dimensions> Dimensions { get; set; } = new();
+	[SectionPosition(6)] public List<TPL_TransportPlacement> TransportPlacement { get; set; } = new();
+	[SectionPosition(7)] public HAN_HandlingInstructions HandlingInstructions { get; set; } = new();
+	[SectionPosition(8)] public TMP_Temperature Temperature { get; set; } = new();
+	[SectionPosition(9)] public List<FTX_FreeText> FreeText { get; set; } = new();
+	[SectionPosition(10)] public List<RFF_Reference> Reference { get; set; } = new();
+	[SectionPosition(11)] public List<SegmentGroup32_SegmentGroup33> SegmentGroup33 {get;set;} = new();
+	[SectionPosition(12)] public List<SegmentGroup32_SegmentGroup35> SegmentGroup35 {get;set;} = new();
+	public ValidationResult Validate()
+	{
+		var validator = new TransactionValidator<SegmentGroup32>(this);
+		validator.Required(x => x.EquipmentDetails);
+		validator.Required(x => x.NumberOfUnits);
+		validator.Required(x => x.TransportMovementDetails);
+		validator.CollectionSize(x => x.Measurements, 1, 9);
+		validator.CollectionSize(x => x.Dimensions, 1, 9);
+		validator.CollectionSize(x => x.TransportPlacement, 1, 9);
+		validator.Required(x => x.HandlingInstructions);
+		validator.Required(x => x.Temperature);
+		validator.CollectionSize(x => x.FreeText, 1, 9);
+		validator.CollectionSize(x => x.Reference, 1, 9);
+		validator.CollectionSize(x => x.SegmentGroup33, 0, 9);
+		validator.CollectionSize(x => x.SegmentGroup35, 0, 99);
+		foreach (var item in SegmentGroup33) validator.Results.AddRange(item.Validate().Errors);
+		foreach (var item in SegmentGroup35) validator.Results.AddRange(item.Validate().Errors);
+		return validator.Results;
+	}
+}
