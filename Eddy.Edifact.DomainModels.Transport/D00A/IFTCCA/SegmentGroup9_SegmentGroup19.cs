88b79a9d31da39e8354d03d7675b86890122b4ea
Dave Woods <dave@solidhouse.com>
2024-08-30 23:22:16 -0600

    batch of EdiFact.Transport

---
+++ Eddy.Edifact.DomainModels.Transport/D00A/IFTCCA/SegmentGroup9_SegmentGroup19.cs
@@ -0,0 +1,24 @@
+using System.Collections.Generic;
+using Eddy.Core.Attributes;
+using Eddy.Core.Validation;
+using Eddy.Edifact.Models.D00A;
+
+namespace Eddy.Edifact.DomainModels.Transport.D00A.IFTCCA;
+
+public class SegmentGroup9_SegmentGroup19 {
+	[SectionPosition(1)] public DGS_DangerousGoods DangerousGoods { get; set; } = new();
+	[SectionPosition(2)] public List<FTX_FreeText> FreeText { get; set; } = new();
+	[SectionPosition(3)] public List<SegmentGroup9__SegmentGroup19_SegmentGroup20> SegmentGroup20 {get;set;} = new();
+	[SectionPosition(4)] public List<SegmentGroup9__SegmentGroup19_SegmentGroup21> SegmentGroup21 {get;set;} = new();
+	public ValidationResult Validate()
+	{
+		var validator = new TransactionValidator<SegmentGroup9_SegmentGroup19>(this);
+		validator.Required(x => x.DangerousGoods);
+		validator.CollectionSize(x => x.FreeText, 1, 99);
+		validator.CollectionSize(x => x.SegmentGroup20, 0, 9);
+		validator.CollectionSize(x => x.SegmentGroup21, 0, 999);
+		foreach (var item in SegmentGroup20) validator.Results.AddRange(item.Validate().Errors);
+		foreach (var item in SegmentGroup21) validator.Results.AddRange(item.Validate().Errors);
+		return validator.Results;
+	}
+}
---
+++ Eddy.Edifact.DomainModels.Transport/D00A/IFTCCA/SegmentGroup9__SegmentGroup16_SegmentGroup17.cs
@@ -0,0 +1,18 @@
+using System.Collections.Generic;
+using Eddy.Core.Attributes;
+using Eddy.Core.Validation;
+using Eddy.Edifact.Models.D00A;
+
+namespace Eddy.Edifact.DomainModels.Transport.D00A.IFTCCA;
+
+public class SegmentGroup9__SegmentGroup16_SegmentGroup17 {
+	[SectionPosition(1)] public MEA_Measurements Measurements { get; set; } = new();
+	[SectionPosition(2)] public List<EQN_NumberOfUnits> NumberOfUnits { get; set; } = new();
+	public ValidationResult Validate()
+	{
+		var validator = new TransactionValidator<SegmentGroup9__SegmentGroup16_SegmentGroup17>(this);
+		validator.Required(x => x.Measurements);
+		validator.CollectionSize(x => x.NumberOfUnits, 1, 9);
+		return validator.Results;
+	}
+}
